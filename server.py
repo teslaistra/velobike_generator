2a74c4c443fe0c771598a7dd4c2bd5901b0bda22
teslaistra <danyefimoff@gmail.com>
2020-12-14 12:36:50 +0300

    time of day added

--- server.py
+++ server.py
@@ -15,16 +15,32 @@ for station in stations['Items']:
     station["IsLocked"] = False
     station["AvailableOrdinaryBikes"] = int(station["FreeOrdinaryPlaces"] / 2)
     station["FreeOrdinaryPlaces"] = int(station["FreeOrdinaryPlaces"] - station["AvailableOrdinaryBikes"])
-print(stations)
-app = FastAPI(title="Hack")
+
+app = FastAPI(title="Velobike")
+
+night = {"actions": [-1, 0, 1], "p": [0.1, 0.8, 0.1]}
+morning = {"actions": [-1, 0, 1], "p": [0.2, 0.6, 0.2]}
+day_peak = {"actions": [-2, -1, 0, 1, 2], "p": [0.2, 0.2, 0.2, 0.2, 0.2]}
+evening = {"actions": [-2, -1, 0, 1, 2], "p": [0.1, 0.3, 0.2, 0.3, 0.1]}
 
 
 def get_state():
-    pass
+    time_now = dt.datetime.now()
+    hour = time_now.hour
+
+    if 0 <= hour <= 7:
+        return night
+    elif 12 >= hour >= 8:
+        return morning
+    elif 13 <= hour <= 17:
+        return day_peak
+    elif 18 <= hour <= 23:
+        return evening
 
 
 @app.get("/")
 async def root():
+    print(get_state())
     return {"message": "Hello World"}
 
 
@@ -33,8 +49,9 @@ def parking():
     global stations
     for station in stations['Items']:
         if station["AvailableOrdinaryBikes"] <= station["TotalOrdinaryPlaces"]:
+            actions, p = get_state()
             station["AvailableOrdinaryBikes"] = station["AvailableOrdinaryBikes"] + int(
-                numpy.random.choice([-1, 0, 1], p=[0.3, 0.4, 0.3]))
+                numpy.random.choice(actions, p))
         if station["AvailableOrdinaryBikes"] < 0:
             station["AvailableOrdinaryBikes"] = 0
         elif station["AvailableOrdinaryBikes"] > station["TotalOrdinaryPlaces"]:
